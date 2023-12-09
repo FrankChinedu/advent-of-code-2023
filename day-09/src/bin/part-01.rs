fe0747de02175b6ff07b0cee21894a214866e1cb
Obi Chinedu Frank <frankieetchy@gmail.com>
2023-12-09 11:59:22 +0100

    day 9 part 1

--- day-09/src/bin/part-01.rs
+++ day-09/src/bin/part-01.rs
@@ -1,20 +1,20 @@
 fn main() {
-    let input = include_str!("./test.txt");
+    let input = include_str!("./input.txt");
     let num = process(input);
     println!("num:{num}");
 }
-fn calc_lagrange(arr: Vec<isize>) -> Vec<isize> {
+fn calc_lagrange(arr: Vec<i128>) -> Vec<i128> {
     arr.iter()
         .enumerate()
-        .map(|(index, value)| lagrange(arr.len() as isize, index as isize, *value))
+        .map(|(index, value)| lagrange(arr.len() as i128, index as i128, *value))
         .collect::<Vec<_>>()
 }
 
-fn lagrange(arr_length: isize, index: isize, value: isize) -> isize {
+fn lagrange(arr_length: i128, index: i128, value: i128) -> i128 {
     let term = arr_length;
 
-    let mut top_divisor = 1_isize;
-    let mut bottom_divisor = 1_isize;
+    let mut top_divisor = 1_i128;
+    let mut bottom_divisor = 1_i128;
 
     for i in 0..arr_length {
         if i == index {
@@ -29,16 +29,19 @@ fn lagrange(arr_length: isize, index: isize, value: isize) -> isize {
     value * (top_divisor / bottom_divisor)
 }
 
-fn process(input: &str) -> isize {
+fn to_integer_array(input: &str) -> Vec<i128> {
+    input
+        .split(' ')
+        .map(|a| a.parse::<i128>().expect("msg"))
+        .collect::<Vec<_>>()
+}
+
+fn process(input: &str) -> i128 {
     input
         .lines()
-        .map(|x| {
-            x.split(' ')
-                .map(|a| a.parse::<isize>().expect("msg"))
-                .collect::<Vec<_>>()
-        })
-        .map(|dataset| calc_lagrange(dataset).iter().sum::<isize>())
-        .sum::<isize>()
+        .map(to_integer_array)
+        .map(|dataset| calc_lagrange(dataset).iter().sum::<i128>())
+        .sum::<i128>()
 }
 
 #[cfg(test)]
@@ -52,22 +55,33 @@ mod test {
 
     #[test]
     fn calc_lagrange_works_1() {
-        let arr = vec![0, 3, 6, 9, 12, 15];
-        let ans = calc_lagrange(arr).iter().sum::<isize>();
+        let ans = calc_lagrange(to_integer_array("0 3 6 9 12 15"))
+            .iter()
+            .sum::<i128>();
         assert_eq!(18, ans);
     }
 
     #[test]
     fn calc_lagrange_works_2() {
-        let arr = vec![1, 3, 6, 10, 15, 21];
-        let ans = calc_lagrange(arr).iter().sum::<isize>();
+        let ans = calc_lagrange(to_integer_array("1 3 6 10 15 21"))
+            .iter()
+            .sum::<i128>();
         assert_eq!(28, ans);
     }
 
     #[test]
     fn calc_lagrange_works_3() {
-        let arr = vec![10, 13, 16, 21, 30, 45];
-        let ans = calc_lagrange(arr).iter().sum::<isize>();
+        let ans = calc_lagrange(to_integer_array("10 13 16 21 30 45"))
+            .iter()
+            .sum::<i128>();
         assert_eq!(68, ans);
+    }
+
+    #[test]
+    fn calc_lagrange_works_4() {
+        let ans = calc_lagrange(
+          to_integer_array("7 10 23 62 147 303 562 967 1585 2545 4127 6933 12171 22103 40826 75939 142603 273520 542191 1113540 2347099")
+        ).iter().sum::<i128>();
+        assert_eq!(4995427, ans); //
     }
 }
