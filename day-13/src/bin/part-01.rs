007b4e3a49f3235fd980a1fdb8f0a2fcbdb8760b
Obi Chinedu Frank <frankieetchy@gmail.com>
2023-12-13 23:15:43 +0100

    day 13 not complete well

--- day-13/src/bin/part-01.rs
+++ day-13/src/bin/part-01.rs
@@ -1,26 +1,62 @@
 fn main() {
     let input = include_str!("./test.txt");
+
     let num = process(input);
     println!("num ={num:?}");
 }
 
-fn get(lines: &Vec<&str>) -> (bool, usize) {
-    let middle = lines.len().div_ceil(2);
-    let (first, second) = lines.split_at(middle);
-
-    let mut result = vec![];
-
-    for (i, x) in first.iter().enumerate() {
-        let diff = middle - (1 + i);
-        match second.get(diff) {
-            Some(y) => {
-                result.push(y == x);
-            }
-            None => continue,
+fn generate_pairs(n: usize) -> Vec<(usize, usize)> {
+    let mut pairs = Vec::new();
+    for i in 1..=n {
+        let min = n;
+        let ma = i + 1;
+        for j in ma..=min {
+            pairs.push((i, j));
         }
     }
-    let bool = result.iter().all(|x| x == &true);
-    (bool, middle)
+    pairs
+}
+
+fn get(lines: &Vec<&str>) -> Option<usize> {
+    let pairs = generate_pairs(lines.len());
+
+    let mut pair: Option<(usize, usize)> = None;
+
+    for current_pair in &pairs {
+        let first = lines[current_pair.0 - 1];
+        let second = lines[current_pair.1 - 1];
+        if first == second {
+            pair = Some(*current_pair);
+            break;
+        }
+    }
+
+    match pair {
+        Some(pair_item) => {
+            println!("pair_item ={pair_item:?} ");
+            Some(pair_item.1.div_ceil(2))
+        }
+        None => None,
+    }
+
+    // let middle = lines.len().div_ceil(2);
+    // let (first, second) = lines.split_at(middle);
+
+    // let mut result = vec![];
+
+    // for (i, x) in first.iter().enumerate() {
+    //     let diff = middle - (1 + i);
+    //     match second.get(diff) {
+    //         Some(y) => {
+    //             result.push(y == x);
+    //         }
+    //         None => continue,
+    //     }
+    // }
+    // println!("\nresult ={result:?}");
+    // println!("  ");
+    // let bool = result.iter().all(|x| x == &true);
+    // (bool, middle)
 }
 
 fn flip_array(lines: &[&str]) -> Vec<String> {
@@ -53,17 +89,23 @@ fn process(input: &str) -> usize {
         .split("\n\n")
         .map(|line| {
             let lines = line.lines().collect::<Vec<_>>();
-            let col = get(&lines);
-
-            if col.0 {
-                col.1 * 100
-            } else {
-                let lines = &lines;
-                let lines = flip_array(lines);
-                let lines = lines.iter().map(|x| x.as_str()).collect::<Vec<_>>();
-                let row = get(&lines);
-                row.1
+            let new_lines = &lines;
+            let new_lines = flip_array(new_lines);
+            let new_lines = new_lines.iter().map(|x| x.as_str()).collect::<Vec<_>>();
+            match get(&new_lines) {
+                Some(val) => val * 100,
+                None => get(&lines).expect("msg now"),
             }
+
+            // if col.0 {
+            // col.1
+            // } else {
+            //     let lines = &lines;
+            //     let lines = flip_array(lines);
+            //     let lines = lines.iter().map(|x| x.as_str()).collect::<Vec<_>>();
+            //     let row = get(&lines);
+            //     row.1
+            // }
         })
         .sum::<usize>()
 }
@@ -72,7 +114,35 @@ mod test {
     use super::*;
     #[test]
     fn works() {
-        let input = include_str!("./test.txt");
+        let input = r#"#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#"#;
+        // let input = include_str!("./test.txt");
+        assert_eq!(405, process(input));
+    }
+
+    #[test]
+    fn test_if_work() {
+        let input = r#"...#.###.######
+..##.##.##.##.#
+####.#....#..#.
+.#...#.#.#.##.#
+.#..#.#.#######
+##..#.#.#######
+.#...#.#.#.##.#"#;
+
         assert_eq!(405, process(input));
     }
 }
---
+++ day-13/src/bin/part-02.rs
@@ -0,0 +1,184 @@
+pub fn p1(input: &str) -> String {
+    let patterns: Vec<Pattern> = input
+        .trim()
+        .split("\n\n")
+        .map(|line| line.parse().unwrap())
+        .collect();
+
+    let sum: i64 = patterns
+        .into_iter()
+        .map(|pattern| pattern.summarize_p1())
+        .sum();
+
+    format!("Sum: {}", sum)
+}
+
+pub fn p2(input: &str) -> String {
+    let patterns: Vec<Pattern> = input
+        .trim()
+        .split("\n\n")
+        .map(|line| line.parse().unwrap())
+        .collect();
+
+    let sum: i64 = patterns
+        .into_iter()
+        .map(|mut pattern| pattern.summarize_p2())
+        .sum();
+
+    format!("Sum: {}", sum)
+}
+
+struct Pattern {
+    grid: HashMap<Pos, char>,
+    width: usize,
+    height: usize,
+}
+
+impl std::fmt::Debug for Pattern {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let lines: Vec<String> = (0..self.height)
+            .map(|y| {
+                (0..self.width)
+                    .map(|x| self.grid.get(&Pos::new(x as i64, y as i64)).unwrap())
+                    .collect()
+            })
+            .collect();
+
+        let lines: Vec<String> = lines.into_iter().rev().collect();
+        f.write_fmt(format_args!("===\n{}\n===", lines.join("\n")))
+    }
+}
+
+impl Pattern {
+    fn summarize_p1(&self) -> i64 {
+        self.try_summarize().next().unwrap()
+    }
+
+    fn summarize_p2(&mut self) -> i64 {
+        let p1_val = self.try_summarize().next().unwrap();
+
+        for x in 0..self.width as i64 {
+            for y in 0..self.height as i64 {
+                let pos = Pos::new(x, y);
+                self.smudge(pos);
+                let maybe_summary = self.try_summarize().filter(|v| *v != p1_val).next();
+                if let Some(v) = maybe_summary {
+                    return v;
+                } else {
+                    self.smudge(pos);
+                }
+            }
+        }
+
+        panic!("We should not get here!")
+    }
+
+    fn smudge(&mut self, p: Pos) {
+        let new = match *self.grid.get(&p).expect("Waaat?") {
+            '.' => '#',
+            '#' => '.',
+            _ => panic!("Oh crap!"),
+        };
+
+        self.grid.insert(p, new);
+    }
+
+    fn try_summarize(&self) -> impl Iterator<Item = i64> + '_ {
+        self.find_vertical_reflections().into_iter().chain(
+            self.find_horizontal_reflections()
+                .into_iter()
+                .map(|v| (self.height as i64 - v) * 100),
+        )
+    }
+
+    fn find_horizontal_reflections(&self) -> impl Iterator<Item = i64> + '_ {
+        (1..self.height as i64).filter(move |y| {
+            let delta_lim = (self.height as i64 - y).min(*y);
+
+            (0..delta_lim).all(move |delta| {
+                let left = self.get_row(y - delta - 1);
+                let right = self.get_row(y + delta);
+
+                left == right
+            }) && delta_lim > 0
+        })
+    }
+
+    fn find_vertical_reflections(&self) -> impl Iterator<Item = i64> + '_ {
+        (1..self.width as i64).filter(move |x| {
+            let delta_lim = (self.width as i64 - x).min(*x);
+
+            (0..delta_lim).all(move |delta| {
+                let left = self.get_col(x - delta - 1);
+                let right = self.get_col(x + delta);
+
+                left == right
+            }) && delta_lim > 0
+        })
+    }
+
+    fn get_row(&self, y: i64) -> Vec<char> {
+        (0..self.width as i64)
+            .map(|x| *self.grid.get(&Pos::new(x, y)).expect("Impossibru"))
+            .collect()
+    }
+
+    fn get_col(&self, x: i64) -> Vec<char> {
+        (0..self.height as i64)
+            .map(|y| *self.grid.get(&Pos::new(x, y)).expect("Impossibru"))
+            .collect()
+    }
+}
+
+impl FromStr for Pattern {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.trim();
+
+        let (width, height) = grid_dimensions(s);
+        let grid = char_grid_iter(s)
+            .map(|(x, y, c)| (Pos::new(x as i64, y as i64), c))
+            .collect();
+
+        Ok(Self {
+            grid,
+            width,
+            height,
+        })
+    }
+}
+
+type Pos = num::Complex<i64>;
+
+use std::{collections::HashMap, str::FromStr};
+
+pub fn char_grid_iter<T: TryFrom<char>>(
+    input: &str,
+) -> impl Iterator<Item = (usize, usize, T)> + '_ {
+    input
+        .trim()
+        .lines()
+        .rev()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.trim()
+                .chars()
+                .enumerate()
+                .filter_map(move |(x, c)| c.try_into().ok().map(|item| (x, y, item)))
+        })
+}
+
+pub fn grid_dimensions(input: &str) -> (usize, usize) {
+    (
+        input.trim().lines().next().unwrap().trim().len(),
+        input.trim().lines().count(),
+    )
+}
+
+fn main() {
+    let input = include_str!("./input.txt");
+    let ans = p2(input);
+
+    println!(" ans ={ans} ")
+}
