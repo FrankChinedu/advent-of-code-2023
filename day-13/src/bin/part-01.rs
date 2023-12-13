e5c07baa3a75d956e7231ee7b06a99f5532b3339
Obi Chinedu Frank <frankieetchy@gmail.com>
2023-12-13 16:27:32 +0100

    day 13 WIp

--- day-13/src/bin/part-01.rs
+++ day-13/src/bin/part-01.rs
@@ -4,16 +4,75 @@ fn main() {
     println!("num ={num:?}");
 }
 
+fn get(lines: &Vec<&str>) -> (bool, usize) {
+    let middle = lines.len().div_ceil(2);
+    let (first, second) = lines.split_at(middle);
+
+    let mut result = vec![];
+
+    for (i, x) in first.iter().enumerate() {
+        let diff = middle - (1 + i);
+        match second.get(diff) {
+            Some(y) => {
+                result.push(y == x);
+            }
+            None => continue,
+        }
+    }
+    let bool = result.iter().all(|x| x == &true);
+    (bool, middle)
+}
+
+fn flip_array(lines: &[&str]) -> Vec<String> {
+    let lines = lines
+        .iter()
+        .map(|x| x.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let mut vec: Vec<Vec<char>> = vec![];
+
+    for (_, item1) in lines.iter().enumerate() {
+        for (y, val) in item1.iter().enumerate() {
+            match vec.get_mut(y) {
+                Some(el) => el.push(*val),
+                None => vec.push(vec![*val]),
+            }
+        }
+    }
+
+    let vec = vec
+        .iter()
+        .map(|x| x.iter().collect::<String>())
+        .collect::<Vec<_>>();
+    // let vec = vec.iter().map(|x| x.as_str()).collect::<Vec<_>>();
+    vec
+}
+
 fn process(input: &str) -> usize {
-    println!("input: {input:?}");
-    0
+    input
+        .split("\n\n")
+        .map(|line| {
+            let lines = line.lines().collect::<Vec<_>>();
+            let col = get(&lines);
+
+            if col.0 {
+                col.1 * 100
+            } else {
+                let lines = &lines;
+                let lines = flip_array(lines);
+                let lines = lines.iter().map(|x| x.as_str()).collect::<Vec<_>>();
+                let row = get(&lines);
+                row.1
+            }
+        })
+        .sum::<usize>()
 }
 #[cfg(test)]
 mod test {
     use super::*;
     #[test]
-    fn universe_is_10_times() {
+    fn works() {
         let input = include_str!("./test.txt");
-        assert_eq!(1030, process(input));
+        assert_eq!(405, process(input));
     }
 }
