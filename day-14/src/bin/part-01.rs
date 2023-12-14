d368f1feb45284b8919b9dd310afca43c3b5b540
Obi Chinedu Frank <frankieetchy@gmail.com>
2023-12-14 11:54:26 +0100

    day 14 WIP

--- day-14/src/bin/part-01.rs
+++ day-14/src/bin/part-01.rs
@@ -1,11 +1,69 @@
 fn main() {
     let input = include_str!("./test.txt");
-    println!("ans {}", process(input));
+    process(input);
+    // println!("ans {}", );
+}
+
+// fn generate_pairs2(n: usize, k: usize) -> Vec<(usize, usize)> {
+//     let mut pairs = Vec::new();
+
+//     for i in 1..=n {
+//         for j in (i + 1)..=std::cmp::min(i + k - 1, n) {
+//             pairs.push((i, j));
+//         }
+//     }
+
+//     pairs
+// }
+fn call(x: usize, y: usize, lines: &mut Vec<Vec<char>>) {
+    if x != 0 {
+        let up_index = x - 1;
+        let up_char = &lines[up_index][y];
+        let char = &lines[x][y];
+        if char == &'#' || up_char == &'#' || char == &'.' {
+            let num = x - 1;
+            return call(num, y, lines);
+        }
+        {
+            let tmp_char = lines[up_index][y];
+            lines[up_index][y] = lines[x][y];
+            lines[x][y] = tmp_char;
+
+            let num = x - 1;
+            call(num, y, lines)
+        }
+    }
 }
 
 fn process(input: &str) -> usize {
-    println!("input: {input}");
-    0
+    let lines: Vec<Vec<char>> = input.lines().map(|x| x.chars().collect()).collect();
+    let len = lines.len();
+    let line_len = lines[0].len();
+
+    let mut lines = lines;
+    for x in (0..len).rev() {
+        for y in 0..line_len {
+            let up_index = x as isize - 1;
+            if x != 0 || up_index.is_positive() {
+                call(x, y, &mut lines)
+            }
+        }
+    }
+    // println!("  ");
+    // for x in &lines {
+    //     println!("{x:?}")
+    // }
+    // println!(" after ");
+    lines
+        .iter_mut()
+        .rev()
+        .enumerate()
+        .map(|(index, char)| {
+            let num = index + 1;
+            let count = char.iter().filter(|y| **y == 'O').count();
+            num * count
+        })
+        .sum::<usize>()
 }
 
 #[cfg(test)]
@@ -15,6 +73,6 @@ mod test {
     #[test]
     fn test_1() {
         let input = include_str!("./test.txt");
-        assert_eq!(20, process(input));
+        assert_eq!(136, process(input));
     }
 }
