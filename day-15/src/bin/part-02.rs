27e926efa9815c551c1d80b46c26c91a7cf335c8
Obi Chinedu Frank <frankieetchy@gmail.com>
2023-12-15 09:40:54 +0100

    day 15 part 2 WIP

---
+++ day-15/src/bin/part-02.rs
@@ -0,0 +1,47 @@
+fn main() {
+    let input = include_str!("./input.txt");
+    println!("ans {}", process(input));
+}
+
+fn get_ascii(char: char) -> u32 {
+    char.into()
+}
+
+fn process(input: &str) -> u32 {
+    input
+        .split(',')
+        .map(|word| {
+            word.chars().map(get_ascii).fold(0, |a, b| {
+                let current_val = a + b;
+                let current_val = current_val * 17;
+                current_val % 256
+            })
+        })
+        .sum::<u32>()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn get_ascii_works() {
+        assert_eq!(72, get_ascii('H'));
+    }
+
+    #[test]
+    fn hash_hash_works() {
+        assert_eq!(52, process("HASH"));
+    }
+
+    #[test]
+    fn test_input_works() {
+        let input = include_str!("./test.txt");
+        assert_eq!(1320, process(input));
+    }
+
+    #[test]
+    fn testa() {
+        assert_eq!(0, process("rn"));
+    }
+}
