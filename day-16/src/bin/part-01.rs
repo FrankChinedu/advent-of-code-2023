0e2ac5791c7e46cbbefd964ce8d6d79b2d5687f1
Obi Chinedu Frank <frankieetchy@gmail.com>
2023-12-17 20:19:18 +0100

    day 16 copied

--- day-16/src/bin/part-01.rs
+++ day-16/src/bin/part-01.rs
@@ -4,12 +4,176 @@ fn main() {
 }
 
 fn process(input: &str) -> usize {
-    println!("input: {input}");
+    let lines = input
+        .lines()
+        .enumerate()
+        .map(|(x, val)| {
+            val.chars()
+                .enumerate()
+                .map(|(y, char)| Tile::new(char, x, y))
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let pos = lines[0][0].pos;
+    let mut lines = lines;
+
+    run(pos, &mut lines);
+
+    for x in lines {
+        println!("{x:?}");
+        println!(" ");
+    }
     0
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Default, Clone, Copy)]
+struct Tile {
+    energized: bool,
+    tile_type: TileType,
+    pos: Pos,
+    from: Direction, // to
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Default, Clone, Copy)]
+struct Pos {
+    x: usize,
+    y: usize,
+}
+
+impl Tile {
+    fn new(char: char, x: usize, y: usize) -> Self {
+        Self {
+            energized: false,
+            tile_type: char.into(),
+            pos: Pos { x, y },
+            ..Default::default()
+        }
+    }
+
+    fn next(&mut self, lines: &mut [Vec<Tile>]) {}
+
+    fn set_from(&mut self, to: Direction) {
+        self.from = to;
+    }
+
+    fn energize(&mut self) {
+        self.energized = true;
+    }
+
+    fn is_valid_next_move(&self, going: Direction, lines: &[Vec<Tile>]) -> bool {
+        let current_pos = self.pos;
+        let outer_length = lines.len();
+        let inner_length = lines.first().expect("has").len();
+        let at_extreme_left = (current_pos.y as isize - 1).is_negative();
+        let at_extreme_right = (current_pos.y + 1) >= inner_length;
+        let at_extreme_top = (current_pos.x as isize - 1).is_negative();
+        let at_extreme_bottom = (current_pos.x + 1) >= outer_length;
+
+        let can_go = |value: bool| !value;
+
+        match going {
+            Direction::Up => can_go(at_extreme_top),
+            Direction::Down => can_go(at_extreme_bottom),
+            Direction::Left => can_go(at_extreme_left),
+            Direction::Right => can_go(at_extreme_right),
+        }
+        // match self
+    }
+
+    fn start_move(&mut self, lines: &mut [Vec<Tile>]) {
+        let tile = self;
+        match tile.tile_type {
+            TileType::EmptySpace => todo!(),
+            TileType::R90Mirror => todo!(),
+            TileType::L90Mirror => todo!(),
+            TileType::HorizontalSplitter => todo!(),
+            TileType::VerticalSplitter => todo!(),
+        }
+    }
+
+    // fn get_next_tile(&self, to: Direction, lines: &mut [Vec<Tile>]) -> Vec<Pos> {
+    //     let pos = self.pos;
+
+    //     match to {
+    //         Direction::Up => {
+    //             let x = pos.x - 1;
+    //             let tile = lines[x][pos.y];
+    //         }
+    //         Direction::Down => {
+    //             let x = pos.x + 1;
+    //             lines[x][pos.y]
+    //         }
+    //         Direction::Left => {
+    //             let y = pos.y - 1;
+    //             lines[pos.x][y]
+    //         }
+    //         Direction::Right => {
+    //             let y = pos.y + 1;
+    //             lines[pos.x][y]
+    //         }
+    //     }
+    // }
+
+    // fn move_from_empty_space()
+}
+
+fn run(pos: Pos, lines: &mut [Vec<Tile>]) {
+    let mut tile = lines[pos.x][pos.y];
+    tile.energize();
+    if tile.is_valid_next_move(tile.from, lines) {
+        return;
+    }
+    // let mut new_tile = tile.get_next_tile(tile.from, lines);
+    // new_tile.set_from(tile.from);
+    //get next tile
+
+    tile.start_move(lines);
+}
+
+impl From<char> for TileType {
+    fn from(val: char) -> Self {
+        match val {
+            '.' => TileType::EmptySpace,
+            '/' => TileType::R90Mirror,
+            '\\' => TileType::L90Mirror,
+            '|' => TileType::VerticalSplitter,
+            '-' => TileType::HorizontalSplitter,
+            _ => unreachable!(" not here"),
+        }
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug, PartialEq, Eq, Default, Clone, Copy)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    #[default]
+    Right,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Default, Clone, Copy)]
+enum TileType {
+    #[default]
+    EmptySpace,
+    R90Mirror,
+    L90Mirror,
+    HorizontalSplitter,
+    VerticalSplitter,
 }
 
 #[cfg(test)]
 mod test {
+    use crate::process;
+
     #[test]
-    fn working() {}
+    fn works() {
+        let input = include_str!("./test.txt");
+        assert_eq!(46, process(input))
+    }
 }
---
+++ day-16/src/bin/part-01r.rs
@@ -0,0 +1,155 @@
+pub fn p1(input: &str) -> String {
+    let grid: Grid = char_grid_iter(input)
+        .map(|(x, y, c)| (Pos::new(x as i64, y as i64), c))
+        .collect();
+
+    let (width, height) = grid_dimensions(input);
+
+    let ans = energized_tiles(
+        &grid,
+        Beam::start(height as i64),
+        width as i64,
+        height as i64,
+    );
+
+    format!("Energized tiles: {}", ans)
+}
+
+pub fn p2(input: &str) -> String {
+    let grid: Grid = char_grid_iter(input)
+        .map(|(x, y, c)| (Pos::new(x as i64, y as i64), c))
+        .collect();
+
+    let (width, height) = grid_dimensions(input);
+
+    let max = (0..width as i64)
+        .flat_map(|x| {
+            [
+                Beam::new(Pos::new(x, 0), NORTH),
+                Beam::new(Pos::new(x, height as i64 - 1), SOUTH),
+            ]
+        })
+        .chain((0..height as i64).flat_map(|y| {
+            [
+                Beam::new(Pos::new(0, y), EAST),
+                Beam::new(Pos::new(width as i64 - 1, y), WEST),
+            ]
+        }))
+        .map(|start| energized_tiles(&grid, start, width as i64, height as i64))
+        .max()
+        .unwrap();
+    format!("Max energization: {}", max)
+}
+
+fn energized_tiles(grid: &Grid, start: Beam, width: i64, height: i64) -> usize {
+    let mut seen = HashSet::new();
+    seen.insert(start.clone());
+
+    let mut beams = vec![start];
+
+    while let Some(beam) = beams.pop() {
+        for next in beam.advance(&grid).into_iter().filter_map(|n| n) {
+            if next.within_contraption(width as i64, height as i64) && !seen.contains(&next) {
+                seen.insert(next.clone());
+                beams.push(next);
+            }
+        }
+    }
+
+    let energized: HashSet<Pos> = seen.into_iter().map(|beam| beam.pos).collect();
+
+    energized.len()
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+struct Beam {
+    pos: Pos,
+    dir: Dir,
+}
+
+impl Beam {
+    fn new(pos: Pos, dir: Dir) -> Self {
+        Self { pos, dir }
+    }
+
+    fn start(height: i64) -> Self {
+        let pos = Pos::new(0, height - 1);
+        let dir = EAST;
+
+        Self { pos, dir }
+    }
+
+    fn advance(&self, grid: &Grid) -> [Option<Self>; 2] {
+        encounter(self.dir, grid.get(&self.pos).cloned().unwrap())
+            .map(|maybe_dir| maybe_dir.map(|dir| Self::new(self.pos + dir, dir)))
+    }
+
+    fn within_contraption(&self, width: i64, height: i64) -> bool {
+        self.pos.re >= 0 && self.pos.re < width && self.pos.im >= 0 && self.pos.im < height
+    }
+}
+
+fn encounter(beam: Dir, tile: char) -> [Option<Dir>; 2] {
+    match tile {
+        '.' => [Some(beam), None],
+        '-' => split([EAST, WEST], beam),
+        '|' => split([NORTH, SOUTH], beam),
+        '/' => [Some(mirror(1, beam)), None],
+        '\\' => [Some(mirror(-1, beam)), None],
+        _ => panic!("Unhandled case"),
+    }
+}
+
+fn split(directions: [Dir; 2], beam: Dir) -> [Option<Dir>; 2] {
+    if directions.contains(&beam) {
+        [Some(beam), None]
+    } else {
+        directions.map(|dir| Some(dir))
+    }
+}
+
+fn mirror(sign: i64, beam: Dir) -> Dir {
+    Dir::new(sign * beam.im, sign * beam.re)
+}
+
+type Grid = HashMap<Pos, char>;
+
+type Pos = Complex<i64>;
+type Dir = Complex<i64>;
+
+static NORTH: Dir = Dir::new(0, 1);
+static SOUTH: Dir = Dir::new(0, -1);
+static EAST: Dir = Dir::new(1, 0);
+static WEST: Dir = Dir::new(-1, 0);
+
+use std::collections::{HashMap, HashSet};
+
+use num::Complex;
+
+pub fn char_grid_iter<T: TryFrom<char>>(
+    input: &str,
+) -> impl Iterator<Item = (usize, usize, T)> + '_ {
+    input
+        .trim()
+        .lines()
+        .rev()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.trim()
+                .chars()
+                .enumerate()
+                .filter_map(move |(x, c)| c.try_into().ok().map(|item| (x, y, item)))
+        })
+}
+
+pub fn grid_dimensions(input: &str) -> (usize, usize) {
+    (
+        input.trim().lines().next().unwrap().trim().len(),
+        input.trim().lines().count(),
+    )
+}
+
+fn main() {
+    let input = include_str!("./input.txt");
+    println!("{}", p2(input));
+}
